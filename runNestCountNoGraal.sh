2efacbd1c4a84ca2e068b900ba5e5a211905de62
James Noble <kjx@ecs.vuw.ac.nz>
2019-01-03 12:28:11 +1300

    adds scripts to run nested benchmark

---
+++ runNestCountNoGraal.sh
@@ -0,0 +1,1 @@
+for i in GraceLibrary/Benchmarks/NestCount?.grace; do ./moth -X -tc  GraceLibrary/Benchmarks/harness0.grace $i 10 100000;  done
---
+++ runNestCountNoTypeChecking.sh
@@ -0,0 +1,1 @@
+for i in GraceLibrary/Benchmarks/NestCount?.grace; do ./moth GraceLibrary/Benchmarks/harness0.grace $i 10 1000000;  done
